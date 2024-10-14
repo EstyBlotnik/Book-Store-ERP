1868bf5fc535a8dedfb200745c36fbb14f4cd286
Esty Blotnik <estyblotnik@gmail.com>
2024-10-14 09:35:11 +0300

    side book

--- functions/index.js
+++ functions/index.js
@@ -10,7 +10,11 @@ function loadingBooks() {
                         <td>$${book.price}</td>
                         <td>Read</td>
                         <td>Update</td>
-                        <td>Delete</td>
+                        <td><button><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+    <path d="M3 6h18M6 6v12a3 3 0 0 0 3 3h6a3 3 0 0 0 3-3V6M9 6V4a3 3 0 0 1 6 0v2"/>
+    <path d="M9 6h6"/>
+    <path d="M12 1v4"/>
+</svg></button></td>
                     </tr>`;
         div.insertAdjacentHTML("beforeend", html);
     }
