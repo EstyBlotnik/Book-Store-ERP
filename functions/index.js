6b2bf5441cd1d36089ad06edbea97a0ec3c5c08f
Esty Blotnik <estyblotnik@gmail.com>
2024-10-14 09:53:45 +0300

    side book

--- functions/index.js
+++ functions/index.js
@@ -8,16 +8,45 @@ function loadingBooks() {
                         <td>${book.id}</td>
                         <td>${book.title}</td>
                         <td>$${book.price}</td>
-                        <td>Read</td>
+                        <td><button id="${book.title}-read">Read</button></td>
                         <td>Update</td>
-                        <td><button><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                        <td><button ><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
     <path d="M3 6h18M6 6v12a3 3 0 0 0 3 3h6a3 3 0 0 0 3-3V6M9 6V4a3 3 0 0 1 6 0v2"/>
     <path d="M9 6h6"/>
     <path d="M12 1v4"/>
 </svg></button></td>
                     </tr>`;
         div.insertAdjacentHTML("beforeend", html);
+        document.getElementById(`${book.title}-read`).addEventListener("click", function () {
+            putOnDisplay(book.title);
+        });
     }
+}
+
+function putOnDisplay(title) {
+    const book = products.books.find(book => book.title === title);
+    console.log(book);
+    const div=document.getElementById("displayBook");
+    let html=`
+    <div class="title">
+                <h3>${book.title}</h3>
+            </div>
+            <div class="bookInfo">
+                <div class="imgFrame"><img src=${book.image}></div>
+                <div class="priceAndRate">
+                    <span class="Price"> price: $ ${book.price}</span>
+                    <div class="rating-container">
+                        <span>Rating:</span>
+                        <div class="rating-bar">
+                            <div class="rating-fill" style="width: ${book.rating*10}%;"></div>
+                        </div>
+                        <span>${book.rating}</span>
+                    </div>
+                </div>
+            </div>
+    `
+    div.innerHTML=html;
+
 }
 
 function deleteBook(id) {
